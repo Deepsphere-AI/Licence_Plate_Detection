c08c88875b3251064a175192c19003135d0e1eee
Palemsandeep <118668380+Palemsandeep@users.noreply.github.com>
2022-12-28 12:43:11 +0530

    Update Licence_Plate_Detection.py

--- Licence_Plate_Detection.py
+++ Licence_Plate_Detection.py
@@ -490,9 +490,9 @@ def main():
 		st.write("")
 	with c63:
 		if len(file_uploaded)>=1:
-		st.write("")
-		st.write("")
-		select6 = st.download_button("Download",output_file,file_name="OutPut.txt",mime='text')
+			st.write("")
+			st.write("")
+			select6 = st.download_button("Download",output_file,file_name="OutPut.txt",mime='text')
 	with c64:
 		st.write("")
 		
